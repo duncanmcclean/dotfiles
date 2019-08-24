e1365337e5b77099f8f8b5aa478e1610ba65d7e6
Duncan McClean <duncan@mcclean.co.uk>
2019-08-24 09:52:00 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -15,7 +15,6 @@ fi
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # Install Homebrew Packages
-
 cd ~
 echo "Installing Homebrew packages"
 
@@ -23,8 +22,8 @@ homebrew_packages=(
   "git"
   "mysql"
   "php"
-  "sqlite"
   "node"
+  "yarn"
 )
 
 for homebrew_package in "${homebrew_packages[@]}"; do
@@ -36,28 +35,34 @@ echo "Installing Homebrew cask packages"
 brew tap caskroom/fonts
 
 homebrew_cask_packages=(
+  "alfred"
+  "android-studio"
   "authy"
   "google-backup-and-sync"
-  "discord"
-  "droplr"
-  "flux"
-  "font-fira-mono"
-  "font-quicksand"
+  "balenaetcher"
+  "brave-browser"
+  "chrome-remote-desktop-host"
+  "docker"
+  "filezilla"
+  "github-desktop"
   "google-chrome"
   "insomnia"
   "iterm2"
+  "obs"
   "onedrive"
   "opendns-updater"
+  "phpstorm"
   "propresenter"
   "rocket"
+  "sketch"
   "slack"
   "spotify"
   "sublime-text"
-  "tableplus"
-  "telegram"
-  "sketch"
-  "visual-studio-code"
+  "sequel-pro"
+  "typora"
+  "virtualbox"
   "vlc"
+  "zoom"
 )
 
 for homebrew_cask_package in "${homebrew_cask_packages[@]}"; do
@@ -71,7 +76,7 @@ mv composer.phar /usr/local/bin/composer
 
 # Install Global Composer Packages
 echo "Installing Global Composer Packages"
-/usr/local/bin/composer global require laravel/installer laravel/valet statamic/cli
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet statamic/cli
 
 # Install Laravel Valet
 echo "Installing Laravel Valet"
@@ -95,6 +100,13 @@ valet restart
 # Installing Global Node Dependecies
 echo "Installing Global Node Dependecies"
 npm install -g @vue/cli
+npm install -g @nuxt/cli
+npm install -g cross-env
+npm install -g heroku
+npm install -g netlify-cli
+
+# Install Tuple
+curl -L https://git.io/tuple-install | bash
 
 # Generate SSH key
 echo "Generating SSH keys"
