270f6941c5b334892e28284748901b177183e8ba
Duncan McClean <duncan@mcclean.co.uk>
2020-08-11 20:46:03 +0100

    add alias

--- aliases.zsh
+++ aliases.zsh
@@ -72,3 +72,18 @@ plslink() {
   echo "\nIn public/vendor/statamic:"
   la public/vendor/statamic
 }
+
+simplink() {
+  if [ "$1" = 'dist' ] || [ "$1" = 'cp' ]; then
+    rm -rf public/vendor/simple-commerce
+    ln -s ~/Sites/simple-commerce/resources/dist public/vendor/simple-commerce
+  elif [ -n "$1" ]; then
+    rm -rf vendor/doublethreedigital/$1
+    ln -s ~/Sites/$1 vendor/doublethreedigital/$1
+  fi
+
+  echo "\nIn vendor/doublethreedigital:"
+  la vendor/doublethreedigital
+  echo "\nIn public/vendor/simple-commerce:"
+  la public/vendor/simple-commerce
+}
