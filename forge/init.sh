9184f5a382a60bfea52a5a91ee14c3ff762b257a
Duncan McClean <duncan@mcclean.co.uk>
2020-08-22 18:14:36 +0100

    Add forge stuff to dotfiles repo

---
+++ forge/init.sh
@@ -0,0 +1,5 @@
+# Bootstrap Necessary Dotfiles for Laravel Forge server
+ln -s /home/forge/dotfiles/forge/.bash_aliases /home/forge/.bash_aliases
+
+# Install cross-env globally
+npm install -g cross-env
