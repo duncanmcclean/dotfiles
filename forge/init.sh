1e9e9a805dd190ccfa6a90b506d6a45a1c55d87b
Duncan McClean <duncan@mcclean.co.uk>
2020-08-22 18:33:30 +0100

    wip

--- forge/init.sh
+++ forge/init.sh
@@ -1,6 +1,6 @@
 # Bootstrap Necessary Dotfiles for Laravel Forge server
 rm /home/forge/.bash_aliases
-ln -s /home/forge/dotfiles/forge/.bash_aliases /home/forge/.bash_aliases
+ln -s /home/forge/.dotfiles/forge/.bash_aliases /home/forge/.bash_aliases
 
 # Install cross-env globally
 sudo npm install -g cross-env
