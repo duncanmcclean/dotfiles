18a8ca2867603dea6aeb9c0e293be3c4994fe19f
Duncan McClean <duncan@steadfastcollective.com>
2020-09-18 15:40:50 +0100

    mys path

--- path.zsh
+++ path.zsh
@@ -1,5 +1,6 @@
 export PATH="$PATH:$HOME/.composer/vendor/bin"
 export PATH=$PATH:/usr/local/mysql/bin/
 export PATH=/usr/local/bin:$PATH
+export PATH="$PATH:$HOME/mys"
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
