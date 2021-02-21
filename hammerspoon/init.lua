2136a99d78f6d0c23d05244f43007b50b7805578
Duncan McClean <duncan@mcclean.co.uk>
2021-02-21 15:49:02 +0000

    wip

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,21 @@
+local hyper = {"cmd", "alt", "ctrl"}
+
+-- Window manager --
+
+hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0.3
+spoon.MiroWindowsManager:bindHotkeys({
+    up = {hyper, "up"},
+    right = {hyper, "right"},
+    down = {hyper, "down"},
+    left = {hyper, "left"},
+    fullscreen = {hyper, "f"}
+})
+
 -- Time to pair...
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function ()
+hs.hotkey.bind({hyper, "T", function ()
     if hs.spotify.isPlaying() then
         hs.spotify.pause()
     end
