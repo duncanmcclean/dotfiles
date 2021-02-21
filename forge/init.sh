168e0dd8809c92fc2e7362a45c2e7356084ba617
Duncan McClean <duncan@mcclean.co.uk>
2021-02-21 16:41:13 +0000

    wip

--- forge/init.sh
+++ forge/init.sh
@@ -1,3 +1,12 @@
-# Bootstrap Necessary Dotfiles for Laravel Forge server
-rm /home/forge/.bash_aliases
-ln -s /home/forge/.dotfiles/forge/.bash_aliases /home/forge/.bash_aliases
+# ------------------------------------------------------------------------------
+# Zsh
+# ------------------------------------------------------------------------------
+
+echo "ZSH_CUSTOM=~/.dotfiles/forge/zsh" >> .zshrc
+
+# ------------------------------------------------------------------------------
+# Aliases
+# ------------------------------------------------------------------------------
+
+# rm /home/forge/.bash_aliases
+# ln -s /home/forge/.dotfiles/forge/.bash_aliases /home/forge/.bash_aliases
