8d06764b7e9d66320d52e94d6574d3f007b8219e
Duncan McClean <duncan@mcclean.co.uk>
2021-02-21 16:46:50 +0000

    wip

--- forge/init.sh
+++ forge/init.sh
@@ -1,12 +1,9 @@
 # ------------------------------------------------------------------------------
-# Zsh
-# ------------------------------------------------------------------------------
-
-echo "ZSH_CUSTOM=~/.dotfiles/forge/zsh" >> .zshrc
-
-# ------------------------------------------------------------------------------
 # Aliases
 # ------------------------------------------------------------------------------
+
+cp /home/forge/.dotfiles/forge/aliases.zsh /home/forge/.zshaliases
+
 
 # rm /home/forge/.bash_aliases
 # ln -s /home/forge/.dotfiles/forge/.bash_aliases /home/forge/.bash_aliases
