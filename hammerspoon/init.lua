bd7f361b2195e5874b2ba16e3ec66e7de363f782
Duncan McClean <duncan@mcclean.co.uk>
2021-04-16 23:50:44 +0100

    wip

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,19 @@
 local hyper = {"cmd", "alt", "ctrl"}
 
+hs.console.darkMode(true)
+
+-- json = require("json")
+
+require("hs.ipc")
+
+-- Ray
+
+-- function ray(dump)
+--     print("we are raying", dump)
+
+--     os.execute("ray 'smth' --large")
+-- end
+
 -- Window manager
 
 hs.loadSpoon("MiroWindowsManager")
@@ -35,3 +49,33 @@ end)
 -- 2. minimise all apps, apart from current one
 -- 3. make current app take up most of screen, apart from border on outside
 -- 4. open Loom window?
+
+hs.hotkey.bind(hyper, "O", function ()
+    local currentWindow = hs.window.focusedWindow()
+    local visibleWindows = 	hs.window.visibleWindows()
+
+    -- ray("hello world")
+
+    -- for window in visibleWindows
+    -- do
+    --     -- print(window.new():title())
+    -- end
+end)
+
+-- Reload Hammerspoon config
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.dotfiles/hammerspoon/", reloadConfig):start()
+hs.alert.show("🥄 Reloaded")
+hs.console.clearConsole()
