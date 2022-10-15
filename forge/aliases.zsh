900b5de9b82381caff2b6f0e83208520c5b35efd
Duncan McClean <duncan@duncanmcclean.com>
2022-10-15 12:31:32 +0100

    update forge git aliases

--- forge/aliases.zsh
+++ forge/aliases.zsh
@@ -1,6 +1,5 @@
 # System
 alias cl="clear"
-alias hello="echo Hiya"
 
 # Laravel & Statmic
 alias a="php artisan"
@@ -20,15 +19,17 @@ alias y="yarn"
 # Git
 alias add="git add"
 alias all="git add ."
+alias fpush="git push -u origin HEAD" # first push, might want to combine this and `push` at some point
 alias push="git push"
 alias pull="git pull"
 alias commit="git commit -m"
+alias empty="git commit --allow-empty"
 alias wip="all && commit 'wip'"
 alias check="git checkout"
-alias newbr="git checkout -b"
-alias branch="git branch"
 alias reset="git reset"
 alias clone="git clone"
 alias init="git init && git add . && git commit -m 'Initial commit'"
 alias status="git status"
-alias diff="git diff -w"
+alias nah="git reset HEAD --hard"
+alias diff="git diff"
+alias unstage="git reset"
