37daa1330d4b9a7952c22a0c61f323d7060442fb
Duncan McClean <duncan@steadfastcollective.com>
2023-01-27 08:01:20 +0000

    Add function to open database in TablePlus
    Copied from https://gist.github.com/calebporzio/20b7d71b8b2690c788f28070f00edcbc?permalink_comment_id=3164939#gistcomment-3164939

--- zsh/laravel.zsh
+++ zsh/laravel.zsh
@@ -4,3 +4,22 @@ alias seed="valet php artisan db:seed"
 alias hor="valet php artisan horizon"
 alias dusk="valet php artisan dusk"
 alias duskf="dusk --filter"
+
+function tpo() {
+  if [ -d '/Applications/TablePlus.app' ]; then
+    TP_DRIVER=$(grep -m1 DB_CONNECTION .env | cut -d '=' -f2)
+    TP_HOST=$(grep -m1 DB_HOST .env | cut -d '=' -f2)
+    TP_USER=$(grep -m1 DB_USERNAME .env | cut -d '=' -f2)
+    TP_PASSWORD=$(grep -m1 DB_PASSWORD .env | cut -d '=' -f2)
+    TP_DATABASE=$(grep -m1 DB_DATABASE .env | cut -d '=' -f2)
+
+    if [ $DRIVER = "sqlite" ]; then
+      open $TP_DATABASE
+    else
+      open $TP_DRIVER://$TP_USRE:$TP_PASSWORD@$TP_HOST/$TP_DATABASE
+    fi
+  else
+    echo "This command uses TablePlus, are you sure it's installed?"
+    echo "Install here: https://tableplus.com/"
+  fi
+}
