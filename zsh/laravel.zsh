45c76baf429065ad79f4b3dd535e81853c9530be
Duncan McClean <duncan@duncanmcclean.com>
2023-05-04 21:47:14 +0100

    Updates to aliases & functions

--- zsh/laravel.zsh
+++ zsh/laravel.zsh
@@ -1,28 +1,21 @@
-alias migrate="valet php artisan migrate"
-alias fresh="valet php artisan migrate:fresh"
-alias seed="valet php artisan db:seed"
-alias hor="valet php artisan horizon"
-alias dusk="valet php artisan dusk"
-alias duskf="dusk --filter"
+# ------------------------------------------------------------------------------
+# Artisan
+# ------------------------------------------------------------------------------
 
-function tpo() {
-  if [ -d '/Applications/TablePlus.app' ]; then
-    TP_DRIVER=$(grep -m1 DB_CONNECTION .env | cut -d '=' -f2)
-    TP_HOST=$(grep -m1 DB_HOST .env | cut -d '=' -f2)
-    TP_USER=$(grep -m1 DB_USERNAME .env | cut -d '=' -f2)
-    TP_PASSWORD=$(grep -m1 DB_PASSWORD .env | cut -d '=' -f2)
-    TP_DATABASE=$(grep -m1 DB_DATABASE .env | cut -d '=' -f2)
-
-    if [ $DRIVER = "sqlite" ]; then
-      open $TP_DATABASE
-    else
-      open $TP_DRIVER://$TP_USRE:$TP_PASSWORD@$TP_HOST/$TP_DATABASE
+a() {
+    if [ -f "./vendor/bin/sail" ]; then
+        sh ./vendor/bin/sail artisan $@
+    elif [ -f "./artisan" ]; then
+        valet php artisan $@
     fi
-  else
-    echo "This command uses TablePlus, are you sure it's installed?"
-    echo "Install here: https://tableplus.com/"
-  fi
 }
+
+alias migrate="a migrate"
+alias fresh="a migrate:fresh"
+alias seed="a db:seed"
+alias hor="a horizon"
+alias dusk="a dusk"
+alias duskf="a dusk --filter"
 
 
 # ------------------------------------------------------------------------------
--- zsh/statamic.zsh
+++ zsh/statamic.zsh
@@ -1,21 +1,5 @@
+alias p="valet php please"
 alias pnew="statamic new"
 alias puser="cp ~/.dotfiles/statamic/duncan@duncanmcclean.com.yaml users/duncan@duncanmcclean.com.yaml"
+alias pusersf="cp ~/.dotfiles/statamic/duncan@steadfastcollective.com.yaml users/duncan@steadfastcollective.com.yaml"
 alias pdeets="valet php please support:details"
-
-
-# ------------------------------------------------------------------------------
-# Setup all the things for a Laravel/Statamic app
-# ------------------------------------------------------------------------------
-
-plssetup() {
-  valet isolate php@8.1
-  composer install
-  cp .env.example .env
-  php artisan key:generate
-  npm install
-  npm run dev
-
-  if [ -f "please" ]; then
-    puser
-  fi
-}
