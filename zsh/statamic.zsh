90bf509a5c0b6e9686ee2357da2dc2d17043bcd6
Duncan McClean <duncan@duncanmcclean.com>
2023-08-19 21:03:24 +0100

    Get rid of the Steadfast user

--- zsh/statamic.zsh
+++ zsh/statamic.zsh
@@ -1,5 +1,4 @@
 alias p="herd php please"
 alias pnew="statamic new"
 alias puser="cp ~/.dotfiles/statamic/duncan@duncanmcclean.com.yaml users/duncan@duncanmcclean.com.yaml"
-alias pusersf="cp ~/.dotfiles/statamic/duncan@steadfastcollective.com.yaml users/duncan@steadfastcollective.com.yaml"
 alias pdeets="herd php please support:details"
