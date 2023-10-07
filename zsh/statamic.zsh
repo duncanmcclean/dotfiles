2544a50ad0a607e8f496cbba9f0d68f3aadaafaf
Duncan McClean <duncan@duncanmcclean.com>
2023-10-07 21:56:29 +0100

    Two emails for Statamic users

--- zsh/statamic.zsh
+++ zsh/statamic.zsh
@@ -1,4 +1,5 @@
 alias p="valet php please"
 alias pnew="statamic new"
-alias puser="cp ~/.dotfiles/statamic/duncan@duncanmcclean.com.yaml users/duncan@duncanmcclean.com.yaml"
+alias puser="cp ~/.dotfiles/statamic/duncan@statamic.com.yaml users/duncan@statamic.com.yaml"
+alias puserme="cp ~/.dotfiles/statamic/duncan@duncanmcclean.com.yaml users/duncan@duncanmcclean.com.yaml"
 alias pdeets="valet php please support:details"
