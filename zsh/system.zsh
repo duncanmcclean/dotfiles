9bb5dd2b7854675ceb0202e26d58f798f082fe6d
Duncan McClean <duncan@duncanmcclean.com>
2023-10-24 17:48:23 +0100

    Add internet alias - borrowing from @jasonvarga

--- zsh/system.zsh
+++ zsh/system.zsh
@@ -6,6 +6,25 @@ alias hosts="sudo nano /etc/hosts"
 alias redflush="redis-cli flushall"
 
 
+internet() {
+    disconnected=false
+
+    while ! ping 8.8.8.8 -c 1 &> /dev/null; do
+        echo '❌ No internet connection.'
+        disconnected=true
+        sleep 1;
+    done;
+
+    # Show notification only if it was ever disconnected, so you
+    # can leave the command running in the background.
+    if $disconnected; then
+        osascript -e 'display notification "Connection restored ✅" with title "Internet"'
+    fi
+
+    echo '✅ Connected to internet.'
+}
+
+
 # ------------------------------------------------------------------------------
 # Finder
 # ------------------------------------------------------------------------------
