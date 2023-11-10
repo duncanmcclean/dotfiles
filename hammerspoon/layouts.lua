384b476fd1d8be3fd7af25ddec5cdcb737738fe4
Duncan McClean <duncan@duncanmcclean.com>
2023-11-10 16:27:38 +0000

    Reconfigure window layouts

--- hammerspoon/layouts.lua
+++ hammerspoon/layouts.lua
@@ -1,9 +1,10 @@
 return {
     {
-        name = 'Standard Code',
+        name = 'Standard',
         cells = {
           { '0,0 40x20' },
-          { '40,0 20x20' },
+          { '40,0 20x10' },
+          { '40,10 20x10' },
         },
         apps = {
           Arc = { cell = 1, open = true },
@@ -13,27 +14,48 @@ return {
           Tinkerwell = { cell = 1 },
           TablePlus = { cell = 1 },
           Slack = { cell = 2, open = true },
-          Things = { cell = 2 },
-          Ray = { cell = 2 },
-          iTerm = { cell = 2 },
-          Obsidian = { cell = 2 },
+          Things = { cell = 3 },
+          Ray = { cell = 3 },
+          iTerm = { cell = 3 },
+          Obsidian = { cell = 3 },
         },
     },
     {
-      name = 'Code Focused',
-      cells = {
-        { '0,0 15x20', positions.sixths.left },
-        { '15,0 53x20', positions.fiveSixths.right },
-      },
-      apps = {
-        Ray = { cell = 1, open = true },
+        name = 'Half Split',
+        cells = {
+            { positions.halves.left },
+            { positions.halves.right },
+        },
+        apps = {
+            Arc = { cell = 1, open = true },
+            Spotify = { cell = 1 },
+            Discord = { cell = 1 },
+            Slack = { cell = 1 },
+            Things = { cell = 1 },
+            TablePlus = { cell = 1 },
+            Ray = { cell = 1 },
+            Code = { cell = 2 },
+            GitHubDesktop = { cell = 2 },
+            Tinkerwell = { cell = 2 },
+            iTerm = { cell = 2 },
+        },
+    },
+    {
+        name = 'Narrow',
+        cells = {
+            { '0,0 15x20', positions.sixths.left },
+            { '15,0 53x20', positions.fiveSixths.right },
+        },
+        apps = {
+        Ray = { cell = 1 },
+        iTerm = { cell = 1 },
         Code = { cell = 2, open = true },
         Arc = { cell = 2 },
         GitHubDesktop = { cell = 2 },
         Slack = { cell = 2 },
         Discord = { cell = 2 },
         Spotify = { cell = 2 },
-      },
+        },
     },
     {
         name = 'Laptop',
