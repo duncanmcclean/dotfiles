9b33a0aa49c62499c369d6e342b5eaed350aaada
Duncan McClean <duncan@duncanmcclean.com>
2023-11-13 22:33:41 +0000

    Add alias for creating git tag

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -10,6 +10,7 @@ alias diff="git diff"
 alias unstage="git reset"
 alias empty="git commit --allow-empty -m 'Empty commit'"
 alias merge="git merge"
+alias gtag="git tag"
 alias ptag="git push --tags"
 alias glt='git describe --tags --abbrev=0' # git latest tag
 alias gcslt='git --no-pager log $(glt)..HEAD --oneline --no-decorate --first-parent --no-merges' # git commits since latest tag
