86265129825280a5064fca85028640b7c26547c5
Duncan McClean <duncan@duncanmcclean.com>
2024-02-12 17:50:16 +0000

    Add command to install everything on an existing Statamic site

--- zsh/statamic.zsh
+++ zsh/statamic.zsh
@@ -23,3 +23,41 @@ pfresh() {
     echo "Site: http://$1.test"
     echo "Control Panel: http://$1.test/cp"
 }
+
+
+# ------------------------------------------------------------------------------
+# Install an existing Statamic site & setup all the things.
+# ------------------------------------------------------------------------------
+
+pinstall() {
+    if [ -f ./.env ]; then
+        echo "Site already has a .env file. Continuing..."
+    elif [ -f ./.env.example ]; then
+        cp .env.example .env
+    else
+        echo "No .env.example file found. Copying from ~/.dotfiles/statamic/.env.example"
+        cp ~/.dotfiles/statamic/.env.example ./.env
+    fi
+
+    ci
+    a key:generate
+
+    if [ -f ./package.json ]; then
+        echo "Running npm install..."
+        npm install
+
+        if grep -q "build" package.json; then
+            echo "Running npm run build..."
+            npm run build
+        elif grep -q "prod" package.json; then
+            echo "Running npm run prod..."
+            npm run prod
+        elif grep -q "production" package.json; then
+            echo "Running npm run production..."
+            npm run production
+        fi
+    fi
+
+    puser
+    osslink statamic/cms ~/Code/Statamic/cms
+}
