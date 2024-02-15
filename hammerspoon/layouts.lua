7d47c02e93b8dba573bdb0eed1997b914fdc434a
Duncan McClean <duncan@duncanmcclean.com>
2024-02-15 12:53:09 +0000

    Change order of layouts

--- hammerspoon/layouts.lua
+++ hammerspoon/layouts.lua
@@ -22,26 +22,6 @@ return {
         },
     },
     {
-        name = 'Centered',
-        cells = {
-          { '5,1 50x18' },
-        },
-        apps = {
-          Arc = { cell = 1 },
-          Discord = { cell = 1 },
-          Spotify = { cell = 1 },
-          Tower = { cell = 1 },
-          Tinkerwell = { cell = 1 },
-          TablePlus = { cell = 1 },
-          Code = { cell = 1 },
-          Slack = { cell = 1 },
-          Things = { cell = 1 },
-          Ray = { cell = 1 },
-          iTerm = { cell = 1 },
-          Bear = { cell = 1 },
-        },
-    },
-    {
         name = 'Full Width',
         cells = {
             { '0,0 60x20' },
@@ -78,6 +58,26 @@ return {
             Tower = { cell = 2 },
             Tinkerwell = { cell = 2 },
             iTerm = { cell = 2 },
+        },
+    },
+    {
+        name = 'Centered',
+        cells = {
+          { '5,1 50x18' },
+        },
+        apps = {
+          Arc = { cell = 1 },
+          Discord = { cell = 1 },
+          Spotify = { cell = 1 },
+          Tower = { cell = 1 },
+          Tinkerwell = { cell = 1 },
+          TablePlus = { cell = 1 },
+          Code = { cell = 1 },
+          Slack = { cell = 1 },
+          Things = { cell = 1 },
+          Ray = { cell = 1 },
+          iTerm = { cell = 1 },
+          Bear = { cell = 1 },
         },
     },
     {
